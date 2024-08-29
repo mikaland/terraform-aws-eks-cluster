205cde157a2d7791164f7e3476e28321a005724e
mikalander <mickaelkremer@gmail.com>
2024-08-29 09:54:08 +0200

    fix scaling config

--- ng-private.tf
+++ ng-private.tf
@@ -19,9 +19,9 @@ resource "aws_eks_node_group" "eks_ng_private" {
   }
 
   scaling_config {
-    desired_size = 2
-    min_size     = 2
-    max_size     = 3
+    desired_size = 3
+    min_size     = 3
+    max_size     = 5
   }
 
   # Desired max percentage of unavailable worker nodes during node group update.
